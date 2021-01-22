eda561510304100b4f81600dbff74da1ce94cd66
TJ Hindman <thind131@gmail.com>
2021-01-21 19:17:56 -0500

    integrated emailjs for users sending email functionality

--- components/AboutUs/AboutUs.js
+++ components/AboutUs/AboutUs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import styles from "./AboutUs.module.css";
 
 export const AboutUs = () => {
   return (
--- pages/contactus.js
+++ pages/contactus.js
@@ -1,3 +1,4 @@
+import emailjs from 'emailjs-com'
 import Head from 'next/head'
 
 import Form from "react-bootstrap/Form";
@@ -14,6 +15,19 @@ const styles = {
 };
 
 const ContactUs = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('diamond_cut_service', 'diamond_cut_template', e.target, 'user_7OS5Lw8Fs2kCclAtHVjUv')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset()
+  }
+
   return (
     <div style={{ height: "100vh", paddingTop: "10vh", backgroundColor: "lightgray" }}>
     <Head>
@@ -23,15 +37,15 @@ const ContactUs = () => {
     <NavBar />
       <h1 style={{ width: "50vw", margin: "0 auto", paddingTop: "2%", fontFamily: "'Crimson Text', serif" }}>Contact Us</h1>
       <hr style={{ width: "40vw" }} />
-      <Form style={styles.formWrap}>
+      <Form style={styles.formWrap} onSubmit={sendEmail}>
         <Form.Group>
           <Form.Label>Name</Form.Label>
-          <Form.Control placeholder="Name" />
+          <Form.Control placeholder="Name" name="name" />
         </Form.Group>
         <Form.Row>
           <Form.Group as={Col}>
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Email" />
+            <Form.Control type="email" placeholder="Email" name="email" />
           </Form.Group>
           <Form.Group as={Col}>
             <Form.Label>Phone Number</Form.Label>
@@ -39,6 +53,7 @@ const ContactUs = () => {
               type="tel"
               pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               placeholder="Phone Number"
+              name="number"
             />
           </Form.Group>
         </Form.Row>
@@ -49,6 +64,7 @@ const ContactUs = () => {
             as="textarea"
             rows="10"
             placeholder="Description of Service Needed"
+            name="description"
           />
         </Form.Group>
         <Form.Row>
